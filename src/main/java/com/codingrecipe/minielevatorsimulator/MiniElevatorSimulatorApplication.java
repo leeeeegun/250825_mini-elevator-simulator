e02eb738599e985273bbd1d7d80cdbc53174deb5
leeeeegun <leeeeegun@naver.com>
2025-08-25 11:48:15 +0900

    Initial commit

---
+++ src/main/java/com/codingrecipe/minielevatorsimulator/MiniElevatorSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.codingrecipe.minielevatorsimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniElevatorSimulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniElevatorSimulatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codingrecipe/minielevatorsimulator/MiniElevatorSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingrecipe.minielevatorsimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniElevatorSimulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
